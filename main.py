712c44dfd8674e2e8f59a088bd339d594d4681f1
keshav316 <139431990+keshav316@users.noreply.github.com>
2026-07-22 15:53:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI, Request, Form
-<<<<<<< HEAD
+
 from fastapi.responses import HTMLResponse, StreamingResponse
-=======
+
 from fastapi.responses import HTMLResponse
->>>>>>> af8b08ecffec43e5dc1c5577f5759f7b6403b00a
+
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from dotenv import load_dotenv
 from pydantic import BaseModel
 import os
-<<<<<<< HEAD
+
 from bot import getAnswer, getAnswerStream # Import the llm, prompt, and chain from bot.py
-=======
+
 from bot import getAnswer # Import the llm, prompt, and chain from bot.py
->>>>>>> af8b08ecffec43e5dc1c5577f5759f7b6403b00a
+
 
 load_dotenv()
 chatbot_db_url = os.getenv("CHATBOT_DB_URL")
@@ -35,7 +35,7 @@ async def user_chat(request: Request, user_input: str = Form(...)):
     chat_history.append({"user": user_input, "bot": bot_response})
     return templates.TemplateResponse("index.html", {"request": request, "chat_history": chat_history})
 
-<<<<<<< HEAD
+
 
 @app.post("/stream")
 async def stream_chat(user_input: str = Form(...)):
@@ -48,8 +48,7 @@ async def stream_chat(user_input: str = Form(...)):
 
     return StreamingResponse(event_generator(), media_type="text/plain")
 
-=======
->>>>>>> af8b08ecffec43e5dc1c5577f5759f7b6403b00a
+
         
     
     
